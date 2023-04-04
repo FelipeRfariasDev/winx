f3001fd8201768a32823a7824d1301c99941c864
Felipe R Farias <feliperfariasdev@gmail.com>
2023-04-04 12:03:56 -0300

    Tests API: Orcamento

---
+++ tests/Feature/OrcamentoApiTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+//use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class OrcamentoApiTest extends TestCase
+{
+    /**
+     * Error create new orcamento web
+     *
+     * @return void
+     */
+    public function test_inserir_orcamento_web_endpoin_api_erro(): void
+    {
+        $orcamento_data = [
+            'nome' => 'Felipe R Farias',
+            'email' => 'feliperfariasdev@gmail.com',
+            'endereco' => 'R. Francelizio Oliveira Coelho Filho, 149 - Praia do PartidoSão Sebastião - SP, 11606-160',
+            'tipo_orcamento' => 'Web',
+            'OpcoesEscolhidas' => 'Chrome;Safari;Firefox',
+            'qtd_paginas' => '10',
+            'autenticacao' => 1,
+            'pagamento' => 1,
+        ];
+
+        $response = $this->postJson("/api/v1/orcamento",$orcamento_data);
+
+        $response->assertStatus(422);
+    }
+
+    /**
+     * Success create new web
+     *
+     * @return void
+     */
+    public function test_inserir_orcamento_web_endpoin_api_sucesso(): void
+    {
+        $orcamento_data = [
+            'nome' => 'Felipe R Farias',
+            'email' => 'feliperfariasdev@gmail.com',
+            'endereco' => 'R. Francelizio Oliveira Coelho Filho, 149 - Praia do PartidoSão Sebastião - SP, 11606-160',
+            'tipo_orcamento' => 'Web',
+            'OpcoesEscolhidas' => 'Chrome;Safari;Firefox',
+            'qtd_paginas' => '10',
+            'autenticacao' => 1,
+            'pagamento' => 1,
+        ];
+
+        $response = $this->postJson("/api/v1/orcamento",$orcamento_data);
+
+        $response->assertStatus(201);
+    }
+}
